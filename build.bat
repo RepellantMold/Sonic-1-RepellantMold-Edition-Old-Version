da6ba5f035060eb903b54d47a8aff4081c005037
Jacob T. Andersen <jaketehspartansalt@gmail.com>
2020-12-24 07:23:39 -0800

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,5 @@
+@echo off
+asm68k /o op+ /o os+ /o ow+ /o oz+ /o oaq+ /o osq+ /o omq+ /m /c /p /o ae- sonic1.asm, s1built.bin , ,sonic1.lst >error.log
+rem rompad.exe s1built.bin 255 0
+convsym sonic1.lst s1built.bin -input asm68k_lst -inopt "/localSign=@ /localJoin=. /ignoreMacroDefs+ /ignoreMacroExp- /addMacrosAsOpcodes+" -a
+fixheadr.exe s1built.bin
